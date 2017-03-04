8e6b9a65ecf5bab250421c21dec1eefa0fdb5f52
redsea111 <redsea111@users.noreply.github.com>
2017-03-04 03:03:56 +0000

    lab 1 linear search added

--- main.cpp
+++ main.cpp
@@ -3,10 +3,17 @@ using namespace std;
 #include <string>
 #include <vector>
 #include "Truck.h"
+#include <cstdlib>//for "exit()" on some systems
+
+int linearSearch(auto data, auto key);//prototype
+
 
 
 int main ()
 {
+	 
+
+
  string name;
  int w;
  vector<Truck> garage;
@@ -51,10 +58,56 @@ for(unsigned int i = 0; i < garage.size(); i++)
   
   cout << garage[i].getWeight() << endl;
 }
-return 0;
+
+  string search_key, input;
+  int result;
+
+   cout<<"Welcome to \"search it\". We first need some input data."<<endl;
+   cout<<"We'll assume the inputs do not have any spaces."<<endl<<endl;
+   cout<<"To end input type the #-character (followed by Enter)"<<endl<<endl;
+
+
+  cout<<"Enter a value to search for: ";
+
+
+   cin>>search_key;
+ 
+    while(search_key != "#")//perform searches until sentinel entered
+    {
+        
+        result = linearSearch(garage,search_key);
+
+        cout<<"  '"<<search_key<<"' was ";
+
+        if (result == -1)
+          cout<<"not found";
+        else
+          cout<<"found at index "<<result;
+
+
+        cout<<endl<<endl<<"Enter a value to search for: ";
+        cin>>search_key; 
+    }
+
+   cout<<endl<<"Program \"search it\" is now finished."<<endl<<endl;
+
+    return 0;
+}
+ 
+ int linearSearch (auto Data, auto key)
+ {
+
+	for( unsigned int i = 0; i <Data.size(); i++)
+	{
+		if (Data[i].getName() == key)
+		{
+			return i;
+
+}
+}
+return -1;
 }
 
- 
 
 
 
